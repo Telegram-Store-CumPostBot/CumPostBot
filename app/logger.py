5b061b07fc107f74a4fe6d6aecad128dc8c39c4f
mihail <pereverzacrazy@gmail.com>
2022-07-06 02:04:31 +0300

    fix levels in cofiguring logger

--- app/logger.py
+++ app/logger.py
@@ -8,13 +8,16 @@ def get_logger(name: str) -> logging.Logger:
 
     cls_handler = logging.StreamHandler()
     cls_handler.setFormatter(formatter)
-    cls_handler.setLevel('DEBUG' if not settings.production else 'WARNING')
+    cls_handler.setLevel(logging.DEBUG if not settings.production else logging.WARNING)
 
     file_handler = logging.FileHandler(settings.logging_file_name)
     file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG if not settings.production else logging.INFO)
 
     logger = logging.getLogger(name)
+
     logger.addHandler(cls_handler)
     logger.addHandler(file_handler)
+    logger.setLevel(logging.DEBUG)
     logger.propagate = False
     return logger
